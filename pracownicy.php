e884002b5b2de018701a170283b4b91c1c32d5aa
Legospeo <114148143+Legospeo@users.noreply.github.com>
2022-11-22 10:00:31 +0100

    Add files via upload

--- pracownicy.php
+++ pracownicy.php
@@ -1,1 +1,30 @@
+<head>
+    <style>
+        table{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th,td{
+            border: 1px solid black;
+            padding: 3px;
+        }
+    </style>
+</head>
+<h1>Tabela <i>pracownicy</i></h1>
 <?php
+$query = 'SELECT * FROM `pracownicy`;';
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0)
+{
+    echo '<table><tr><th>Id_pracownika</th><th>Nazwisko</th><th>Imie</th><th>Id_stanowisko</th><th>Miasto</th><th>Data zatrudnienia</th><th>Wynagrodzenie</th></tr>';
+    while ($row = mysqli_fetch_assoc($result))
+    {
+        echo '<tr>';
+        foreach($row as $key => $value)
+        {
+            echo '<td>'.$value.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
--- stanowiska.php
+++ stanowiska.php
@@ -1,1 +1,30 @@
+<head>
+    <style>
+        table{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th,td{
+            border: 1px solid black;
+            padding: 3px;
+        }
+    </style>
+</head>
+<h1>Tabela <i>czytelnicy</i></h1>
 <?php
+$query = 'SELECT * FROM `stanowiska`;';
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0)
+{
+    echo '<table><tr><th>Id_stanowiska</th><th>Nazwa</th></tr>';
+    while ($row = mysqli_fetch_assoc($result))
+    {
+        echo '<tr>';
+        foreach($row as $key => $value)
+        {
+            echo '<td>'.$value.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
