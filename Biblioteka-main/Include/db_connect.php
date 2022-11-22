c7aafc7ff92fb544021158c1ac5d4b01fd3180f6
Legospeo <114148143+Legospeo@users.noreply.github.com>
2022-11-22 09:31:03 +0100

    Add files via upload

---
+++ Biblioteka-main/Include/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "Biblioteka";
+$password = "biblioteka";
+$database = "biblioteka";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
